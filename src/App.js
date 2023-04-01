bdc9610a02bc696cdfef2493e0baa8d7a1396220
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-04-01 22:04:45 +0530

    Started setup for portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useContext } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import { ThemeContext } from './contexts/ThemeContext';
+import { Main, BlogPage, ProjectPage } from './pages';
+import { BackToTop } from './components';
+import ScrollToTop from './utils/ScrollToTop';
+
 import './App.css';
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <ScrollToTop />
+      <Routes>
+        <Route
+          path='/'
+          element={<Main />}
+        />
+        <Route
+          path='/blog'
+          element={<BlogPage />}
+        />
+        <Route
+          path='/projects'
+          element={<ProjectPage />}
+        />
+      </Routes>
+      <Navigate to='/' />
+      <BackToTop />
     </div>
   );
 }
