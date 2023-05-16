613d977316c81eecda357a05f640d35d1d37c0f1
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-05-16 20:30:30 +0530

    Created data for about section

---
+++ src/data/aboutData.js
@@ -0,0 +1,7 @@
+export const aboutData = {
+  title: 'Who I am',
+  description1: `I am a passionate Full Stack Web Developer specializing in the MERN (MongoDB, Express.js, React, Node.js) stack. With a strong background in designing, developing, and maintaining projects, I take pride in crafting clean and efficient code. My expertise in React, Redux, Node.js, and Express.js allows me to create dynamic and user-friendly interfaces while ensuring robust server-side logic and data management using MongoDB.`,
+  description2:
+    'I also have a solid understanding of Data Structures & Algorithms, enabling me to optimize code performance and find innovative solutions to complex challenges. As a lifelong learner, I eagerly embrace new technologies and programming principles to continuously enhance my skills and deliver cutting-edge solutions. Collaboration and effective communication are at the heart of my work ethic, enabling me to thrive in team environments and contribute to successful projects.',
+  image: 2,
+};
