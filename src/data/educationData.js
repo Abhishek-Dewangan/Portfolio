8d1973c6f8cea710298d7c59e71c0106c122cbaa
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-05-20 20:20:40 +0530

    Added education data in data file

---
+++ src/data/educationData.js
@@ -0,0 +1,30 @@
+export const educationData = [
+  {
+    id: 1,
+    institution: 'Masai School',
+    course: 'Full Stack Web Development',
+    startYear: 'Dec 2021',
+    endYear: 'present',
+  },
+  {
+    id: 2,
+    institution: 'Guru Ghasidas University( a central university)',
+    course: `Master's in Electronics`,
+    startYear: '2018',
+    endYear: '2020',
+  },
+  {
+    id: 3,
+    institution: 'Guru Ghasidas University( a central university)',
+    course: `Bachlore's in Electronics`,
+    startYear: '2015',
+    endYear: '2018',
+  },
+  // {
+  //   id: 4,
+  //   institution: 'Bharat Mata H.S. School Bilaspur',
+  //   course: 'Schooling',
+  //   startYear: '2008',
+  //   endYear: '2014',
+  // },
+];
