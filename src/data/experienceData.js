945e4d6ec95abed591cc6288a73430c3578600d2
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-05-23 20:01:59 +0530

    Added experience data in data file

---
+++ src/data/experienceData.js
@@ -0,0 +1,26 @@
+export const experienceData = [
+  {
+    id:1,
+    company:'CrioDo',
+    jobtitle:'Project Engineer (QA)',
+    startYear: 'Apr 2023 - july 2023',
+  },
+  {
+    id: 2,
+    company: 'Masai School',
+    jobtitle: 'Full Stack Web Developer Training',
+    startYear: 'Feb 2022 - March 2023',
+  },
+  {
+    id: 3,
+    company: 'Masai School',
+    jobtitle: 'Student Guide',
+    startYear: 'Apr 2022 - May 2022',
+  },
+  {
+    id: 4,
+    company: 'Chhattisgarh State Power Transmission Company Limited',
+    jobtitle: 'Research Trainee',
+    startYear: 'Jan 2020 - Apr 2020',
+  },
+];
