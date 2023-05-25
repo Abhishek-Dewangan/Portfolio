8f7b0dbbbbe9b6370100244ac4ea305ec4e9a0c4
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-05-25 21:45:43 +0530

    Created project data file

---
+++ src/data/projectsData.js
@@ -0,0 +1,114 @@
+// logos: 'React | Node-Js | Express-Js | MongoDB | HTML | CSS | Chakra-UI',
+import sephora from '../assets/png/sephora.jpg';
+import indiamart from '../assets/png/indiamart.jpg';
+import bigBasket from '../assets/png/big-basket.jpg';
+import kimaye from '../assets/png/kimaye.png';
+import myhours from '../assets/png/myhours.png';
+import sugarcosmetic from '../assets/png/sugarcosmetic.png';
+import flipkart from '../assets/png/flipkart.png';
+
+export const projectsData = [
+  {
+    id: 7,
+    projectName: 'Flipkart Clone',
+    projectDesc:
+      'Signup | Signin | Signout | Easy user cart | wishlist | order management | Search functionality | User-friendliness | Interactive product sliders and pages',
+    tags: [
+      'HTML',
+      'CSS',
+      'React',
+      'Redux',
+      'Node Js',
+      'Express Js',
+      'MongoDB',
+      'Bootstrap',
+    ],
+    logos:
+      'HTML | CSS | JavaScript | React | MongoDB | NodeJS | Express| Bootstrap',
+    code: 'https://github.com/Abhishek-Dewangan/Flipkart-Clone',
+    demo: 'https://flipkart-v1.netlify.app/',
+    image: flipkart,
+  },
+  {
+    id: 2,
+    projectName: 'Kimaye Clone',
+    projectDesc:
+      'Authenticated Login-Signup | User Cart | Smooth Landing page with Sliders Checkout Page, Payment Gateway Learn & Grow Page',
+    tags: ['React JS', 'Node JS', 'Express JS', 'MongoDB', 'Boostrap', 'Git'],
+    logos:
+      'HTML | CSS | JavaScript | React | Redux | MongoDB | NodeJS | Express| Bootstrap',
+    code: 'https://github.com/Abhishek-Dewangan/Kimaye-Clone',
+    demo: 'https://kimaye-cloned.netlify.app/',
+    image: kimaye,
+  },
+  {
+    id: 3,
+    projectName: 'Sugar-Cosmetics Clone',
+    projectDesc:
+      'Authenticated Login-SignUp | Real Time Tracking of Projects | Handeling Multiple Clients and Projects | Adding multiple Team and Members',
+    tags: ['React', 'MongoDB', 'NodeJS', 'Express JS'],
+    logos:
+      'HTML | CSS | JavaScript | React | Redux | MongoDB | NodeJS | Express| Chakra-UI | Swiper',
+    code: 'https://github.com/Abhishek-Dewangan/Sugar-Cosmetic-Clone',
+    demo: 'https://sugar-cosmatics.netlify.app/',
+    image: sugarcosmetic,
+  },
+  {
+    id: 1,
+    projectName: 'Big-Basket Clone',
+    projectDesc:
+      'Login-SignUp | Add and Remove the products | Search for products | Completely responsive website for both desktop and mobile devices',
+    tags: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+    logos: 'HTML | CSS | JavaScript | Swiper-JS | Bootstrap',
+    code: 'https://github.com/Abhishek-Dewangan/Big-Basket',
+    demo: 'https://bigbasket-clone26.netlify.app/',
+    image: bigBasket,
+  },
+  {
+    id: 4,
+    projectName: 'Sephora Clone',
+    projectDesc:
+      'Login-SignUp| Smooth landing page, Products pages, Cart page, Address page | Add to Cart | User Friendly | Responsive Website',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    logos: 'HTML | CSS | JavaScript | Git | Bootstrap',
+    code: 'https://github.com/Abhishek-Dewangan/SephoraProject',
+    demo: 'https://suspicious-mccarthy-90cb20.netlify.app/',
+    image: sephora,
+  },
+  {
+    id: 5,
+    projectName: 'Indiamart Clone',
+    projectDesc:
+      'Login Signup | Smooth landing page, Product page, About page, ContactUs page | Smooth products purchase flow | User Friendly',
+    tags: ['Html', 'css', 'javascript'],
+    logos: 'HTML | CSS | JavaScript | React | Redux | Material-UI | Bootstrap',
+    code: 'https://github.com/Abhishek-Dewangan/Indiamart-Clone',
+    demo: 'https://india-mart-clone.netlify.app/',
+    image: indiamart,
+  },
+  {
+    id: 6,
+    projectName: 'MyHours Clone',
+    projectDesc:
+      'Authenticated Login-SignUp | Real Time Tracking of Projects | Handeling Multiple Clients and Projects | Adding multiple Team and Members',
+    tags: ['React', 'MongoDB', 'NodeJS', 'Express JS'],
+    logos:
+      'HTML | CSS | JavaScript | React | MongoDB | NodeJS | Express| Chakra-UI',
+    code: 'https://github.com/Abhishek-Dewangan/My-Hours-Clone',
+    demo: 'https://my-hours-clone.netlify.app/',
+    image: myhours,
+  },
+];
+
+//schema for entwreing data
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
