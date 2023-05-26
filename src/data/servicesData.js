7adaf66adc1b446c67776b64750dbf0656607328
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-05-26 16:32:48 +0530

    Added services data in data file

---
+++ src/data/servicesData.js
@@ -0,0 +1,94 @@
+/* eslint-disable */
+import { IconContext } from 'react-icons';
+import { MdOutlineMiscellaneousServices } from 'react-icons/md';
+import { BsCodeSlash, BsClipboardData } from 'react-icons/bs';
+import {
+  AiOutlineMail,
+  AiFillAudio,
+  AiOutlineLaptop,
+  AiOutlineMobile,
+  AiOutlineCloudServer,
+  AiOutlineGithub,
+} from 'react-icons/ai';
+import {
+  FaInternetExplorer,
+  FaChalkboardTeacher,
+  FaCameraRetro,
+  FaPinterest,
+  FaVideo,
+  FaTabletAlt,
+  FaRegNewspaper,
+} from 'react-icons/fa';
+import { DiResponsive } from 'react-icons/di';
+
+export const servicesData = [
+  // {
+  //     id: 1,
+  //     title: 'Marketing',
+  //     icon: <BiShoppingBag/>
+  // },
+  // {
+  //     id: 2,
+  //     title: 'Content Writing',
+  //     icon: <BiPencil />
+  // },
+  {
+    id: 6,
+    title: 'Web Design',
+    icon: <BsCodeSlash />,
+  },
+  {
+    id: 3,
+    title: 'API Development',
+    icon: <AiOutlineCloudServer />,
+  },
+  {
+    id: 8,
+    title: 'Responsive Design',
+    icon: <AiOutlineLaptop />,
+  },
+  {
+    id: 4,
+    title: 'Version Control',
+    icon: <AiOutlineGithub />,
+  },
+  // {
+  //   id: 5,
+  //   title: 'Tutoring',
+  //   icon: <FaChalkboardTeacher />,
+  // },
+  {
+    id: 7,
+    title: 'Integration Services',
+    icon: <MdOutlineMiscellaneousServices />,
+  },
+  // {
+  //     id: 9,
+  //     title: 'Video Editing',
+  //     icon: <FaVideo />
+  // },
+  // {
+  //     id: 10,
+  //     title: 'Data Entry',
+  //     icon: <BsClipboardData />
+  // },
+  // {
+  //     id: 11,
+  //     title: 'Ebook Writing',
+  //     icon: <FaTabletAlt />
+  // },
+  // {
+  //     id: 12,
+  //     title: 'Audio Transcription',
+  //     icon: <AiFillAudio />
+  // },
+  // {
+  //     id: 13,
+  //     title: 'Blog Posting',
+  //     icon: <FaRegNewspaper />
+  // },
+];
+
+// Uncomment your required service.
+// Couldn't find the required services? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
+// You can also add on your own 😉.
