0f45491b7102def871706557374b52106e4dce67
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-05-30 20:19:59 +0530

    Added skills data in data file

---
+++ src/data/skillsData.js
@@ -0,0 +1,21 @@
+export const frontendSkill = [
+  'HTML',
+  'CSS',
+  'Javascript',
+  'React',
+  'Redux',
+  'Bootstrap',
+  'MaterialUI',
+  'ChakraUI',
+];
+
+export const backendSkill = [
+  'nodejs',
+  'ExpressJS',
+  'MongoDB',
+  'Mongoose',
+  'Java',
+  'Selenium',
+  'Postman',
+  'VSCode',
+];
