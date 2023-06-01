71bdf9878eb31764de2e41a588169490ebb936c2
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-06-01 16:45:18 +0530

    Added social media links in data file

---
+++ src/data/socialsData.js
@@ -0,0 +1,7 @@
+export const socialsData = {
+  github: 'https://github.com/Abhishek-Dewangan',
+  linkedIn: 'https://www.linkedin.com/in/abhishek-dewangan-319345218/',
+  medium: 'https://medium.com/@callmeabhi1997',
+  hackerrank: 'https://www.hackerrank.com/callmeabhi1997',
+  leetcode: 'https://leetcode.com/callmeabhi1997/',
+};
